579f4669c76034a6e266494b7bc803026357e0a8
cang25 <137566275+cang25@users.noreply.github.com>
2023-08-07 17:47:36 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+const {reviewData} = window; 
+
+//for debugging 
+console.log("REVIEWS");
+
+function displayReviews(reviewData){
+    var reviewPage = document.getElementById("reviewsDis");
+    reviewData.forEach((review)=>{
+        var card = document.createElement("div");
+        var cardHeader = document.createElement("div");
+        
+        var name = document.createElement("h5");
+        var date = document.createElement("p");
+        var rate = document.createElement("p");
+        var reviewContent = document.createElement("p");
+
+
+        name.textContent = review.name; 
+        date.textContent = review.date;
+        rate.textContent = review.rating; 
+        reviewContent.textContent = review.text;
+     
+        card.className = "card";
+        cardHeader.className = "review";
+
+        cardHeader.appendChild(name);
+        cardHeader.appendChild(date);
+        cardHeader.appendChild(rate);
+        cardHeader.appendChild(reviewContent);
+        card.appendChild(cardHeader);
+        reviewPage.appendChild(card);
+        
+    });
+   
+}
+
+function resetReviews(){
+    var reviewPage = document.getElementById("reviewsDis");
+    reviewPage.innerHTML = "";
+}
+
+function addReview() {
+    var form = document.getElementById("newRev");
+    form.addEventListener("submit", (event) =>{
+        event.preventDefault();
+        
+        var name = document.getElementById("name").value;
+        var dateToday = new Date().toLocaleDateString();
+        var rate = document.getElementById("rating").value;
+        var content = document.getElementById("postReview").value;
+
+        var newReview = {
+            name: name, 
+            date: dateToday,
+            rating: rate,
+            text: content,
+           
+        };
+
+        reviewData.push(newReview);
+
+        console.log(reviewData);
+        resetReviews();
+        form.reset();
+        displayReviews(reviewData);
+    })
+}
+
+function ValidEmail() {
+    
+    var form = document.getElementById("newletter-sign-up");
+    var emailInput = document.getElementById('email');
+    var emailError = document.getElementById('errorMsg');
+
+    form.addEventListener('submit', (event) =>{
+        if (!validateEmail(emailInput.value)) {
+            event.preventDefault();
+        } else {
+            emailError.textContent = '';
+        }
+});
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+    
+}
+
+function onLoad(){
+    console.log("Loaded");
+    displayReviews(reviewData); 
+    addReview();
+}
+
+window.addEventListener("load", onLoad);
---
+++ data.js
@@ -0,0 +1,37 @@
+window.reviewData = [
+    {
+        name: "Leslie Knope",
+        date: "1/25/20",
+        rating: "4",
+        text: "\"Can be challenging, but it really brings out my inner survival skills. Love it! Would recommend to my fellow gatherers!\""
+
+    },
+    {
+        name: "Jerry Gergich",
+        date: "5/11/20",
+        rating: "5",
+        text: "\"Great for the everyone in the family!\""
+
+    },
+    {
+        name: "Ron Swanson",
+        date: "7/29/21",
+        rating: "5",
+        text: "\"You can choose to have friends, or you can choose to win.\""
+
+    },
+    {
+        name: "April Ludgate",
+        date: "8/5/22",
+        rating: "3",
+        text: "\"I hate most board games but this one is tolerable.\""
+
+    },
+    {
+        name: "Tom Haverford",
+        date: "4/5/23",
+        rating: "3",
+        text: "\"Sometimes you have to gather a little, so you can win a lot\""
+
+    }
+];
